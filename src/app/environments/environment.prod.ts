3504f48e220e4fe35300e600eb21bd098a177ec4
Evalton Gomes <36282520+evalton2017@users.noreply.github.com>
2026-05-14 05:52:25 -0300

    feat: server

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   versao: '1.0.0',
   production: false,
   url: 'https://busca-car.api.br/agroprods/api/v1/prods',
-  postLogoutRedirectUri: 'http://localhost:4200/',
+  postLogoutRedirectUri: 'http://162.159.140.98/',
 };
