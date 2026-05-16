15ffaee5331414b258dd18b606793022a4ea362c
Evalton Gomes <36282520+evalton2017@users.noreply.github.com>
2026-05-16 18:43:06 -0300

    feat: correção url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   versao: '1.0.0',
   production: true,
-  url: 'https://busca-car.api.br/agroprods/api/v1/prods',
+  url: 'https://busca-car.api.br/agroprods/api/v1',
   postLogoutRedirectUri: 'https://agropodes-app-jcmmv.ondigitalocean.app',
 };
